455e1a49cad8e829578367a03457ad4996d1b29f
Omer Adam <127669568+omerad1@users.noreply.github.com>
2023-06-06 20:53:45 +0300

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+var year = new Date().getFullYear();
+
+function footer() {
+  return (
+    <footer>
+      <p>Created by Omer in {year} 🧙🏼</p>
+    </footer>
+  );
+}
+export default footer;
---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Note from "./Note";
+import Header from "./Header";
+import React from "react";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
